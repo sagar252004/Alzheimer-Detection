acd08f48774a852d6187f561af89f0202bacefee
sagar252005 <sagarrv152@gmail.com>
2026-01-26 14:08:11 +0530

    updated app.py port

--- app.py
+++ app.py
@@ -83,3 +83,8 @@ def result(request: Request):
             "message": None
         }
     )
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
